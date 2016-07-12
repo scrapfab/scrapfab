2deb35c56bd51a582a70129d45ac49f3b4315a76
Dmitry Kolobov <dmkolobov@gmail.com>
2016-07-11 23:43:47 -0600

    Use offsetWidth and offsetHeight for getting image dimensions.

--- resources/public/js/gallery.js
+++ resources/public/js/gallery.js
@@ -1,13 +1,13 @@
 // given an HTMLElement image element, return its
 // width and height
 function getImageDimensions( img ){
-    return [img.clientWidth, img.clientHeight];
+    return [img.offsetWidth, img.offsetHeight];
 }
 
 // given an ideal gallery row height and dimensions for an image,
 // scale the dimensions so that the image fits the row
 function fitDimensionsToRow( rowHeight, dim ){
-    var factor = rowHeight / dim[0];
+    var factor = rowHeight / dim[1];
     return [dim[0] * factor, dim[1] * factor];
 }
 
@@ -35,16 +35,21 @@ function gallery( element ){
     var aspects = _.map(sizes, getNormalizedRatios);
 
     var idealRowHeight = window.innerHeight / 2;
-    var idealRowWidth = window.innerWidth;
+    var idealRowWidth = element.offsetWidth;
 
     var idealSizes = _.map(sizes, _.partial(fitDimensionsToRow, idealRowHeight));
     var rowCount = findRowCount(idealRowWidth, idealSizes);
 
     var newGallery = document.createElement("div");
 
+    console.log(idealSizes);
+
     var partitions = linear_partition(aspects, rowCount);
 
+    console.log(partitions);
+
     for( var partition of partitions ){
+
         var rowImages = _.map(partition, function(aspect){
            var index = _.indexOf(aspects, aspect);
 
@@ -53,13 +58,8 @@ function gallery( element ){
            var image = _.pullAt(images, [index])[0];
            var dims = _.pullAt(idealSizes, [index])[0];
 
-           console.log(idealSizes);
-           console.log(dims);
-
            image.width = dims[0];
            image.height = dims[1];
-
-           console.log(image);
 
            return image;
         });
